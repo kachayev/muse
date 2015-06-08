fe688a055ac529131a820801f91019437ccde002
Alexey Kachayev <kachayev@gmail.com>
2015-06-08 23:21:14 +0300

    Default gpg signature settings

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :global-vars {*warn-on-reflection* false}
   :plugins [[cider/cider-nrepl "0.8.2"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :signing {:gpg-key "kachayev@gmail.com"})
