8cbc1e7070ed2f6dd63769a5a02d3213d5417a51
Alexey Kachayev <kachayev@gmail.com>
2015-06-08 11:54:36 +0300

    Basic implementation of core functionality

---
+++ src/muse/core.clj
@@ -0,0 +1,136 @@
+(ns muse.core
+  (:require [clojure.core.async :as async :refer [go <! >! <!! >!!]]))
+
+(defprotocol DataSource
+  (fetch [this]))
+
+(defprotocol LabeledSource
+  (resource-id [this]))
+
+(defprotocol BatchedSource
+  (fetch-multi [this resources]))
+
+;; xxx: what to do with "reify"?
+(defn resource-name [v]
+  (.getName (.getClass v)))
+
+(defprotocol MuseAST
+  (inject [this env])
+  (done? [this]))
+
+(defprotocol ComposedAST
+  (compose-ast [this f]))
+
+(defrecord MuseDone [value]
+  MuseAST
+  (done? [_] true)
+  (inject [this _] this))
+
+(defn cache-id [res]
+  (if (satisfies? LabeledSource res)
+    (resource-id res)
+    (:id res)))
+
+(defn cache-path [res]
+  [(resource-name res) (cache-id res)])
+
+(defn cached-or [env res]
+  (let [cached (get-in env (cons :cache (cache-path res)) ::not-found)]
+    (if (= ::not-found cached)
+      res
+      (MuseDone. cached))))
+
+(defn inject-into [env node]
+  (if (satisfies? DataSource node)
+    (cached-or env node)
+    (inject node env)))
+
+(defrecord MuseMap [f values]
+  ComposedAST
+  (compose-ast [_ f2] (MuseMap. (comp f2 f) values))
+  MuseAST
+  (done? [_] false)
+  (inject [_ env]
+    (let [next (map (partial inject-into env) values)]
+      (if (= (count next) (count (filter done? next)))
+        (MuseDone. (apply f (map :value next)))
+        (MuseMap. f next)))))
+
+(defn assert-ast! [ast]
+  (assert (or (satisfies? MuseAST ast)
+              (satisfies? DataSource ast))))
+
+(defrecord MuseFlatMap [f values]
+  MuseAST
+  (done? [_] false)
+  (inject [_ env]
+    (let [next (map (partial inject-into env) values)]
+      (if (= (count next) (count (filter done? next)))
+        (let [result (apply f (map :value next))]
+          ;; xxx: refactor to avoid dummy leaves creation
+          (if (satisfies? DataSource result) (MuseMap. identity [result]) result))
+        (MuseFlatMap. f next)))))
+
+(defrecord MuseValue [value]
+  ComposedAST
+  (compose-ast [_ f2] (MuseValue. (f2 value)))
+  MuseAST
+  (done? [_] false)
+  (inject [_ env] (MuseDone. value)))
+
+(defn value [v]
+  (MuseValue. v))
+
+(defn fmap [f muse & muses]
+  (if (and (not (seq muses))
+           (satisfies? ComposedAST muse))
+    (compose-ast muse f)
+    (MuseMap. f (cons muse muses))))
+
+;; xxx: make it compatible with algo.generic and cats libraries
+(defn flat-map [f muse & muses]
+  (MuseFlatMap. f (cons muse muses)))
+
+;; xxx: use macro instead for auto-partial function building
+(def <$> fmap)
+(defn >>= [muse f] (flat-map f muse))
+
+;; xxx: better high-level API
+(defn collect [muses]
+  (apply (partial fmap vector) muses))
+
+(defn traverse [f coll-muse]
+  (flat-map #(collect (map f %)) coll-muse))
+
+(defn next-level [ast-node]
+  (if (satisfies? DataSource ast-node)
+    (list ast-node)
+    (if-let [values (:values ast-node)] (mapcat next-level values) '())))
+
+(defn fetch-group [[rname [head & tail]]]
+  (go [rname
+       (if (not (seq tail))
+         (let [res (<! (fetch head))] {(cache-id head) res})
+         (if (satisfies? BatchedSource head)
+           (<! (fetch-multi head tail))
+           (let [all-res (map (fn [[k v]] (first v)) (group-by cache-id (cons head tail)))]
+             ;; xxx: refactor
+             (<! (go (let [ids (map cache-id all-res)
+                           fetch-results (<! (async/map vector (map fetch all-res)))]
+                       (into {} (map vector ids fetch-results))))))))]))
+
+;; xxx: catch & propagate exceptions
+(defn run! [ast]
+  (go
+   (loop [ast-node ast cache {}]
+     (let [fetches (next-level ast-node)]
+       (if (not (seq fetches))
+         (:value ast-node) ;; should be Done, throw exception otherwise
+         (let [by-type (group-by resource-name fetches)
+               fetch-groups (<! (async/map vector (map fetch-group by-type)))
+               to-cache (into {} fetch-groups)
+               next-cache (into cache to-cache)]
+           (recur (inject-into {:cache next-cache} ast-node) next-cache)))))))
+
+(defn run!! [ast]
+  (<!! (run! ast)))
