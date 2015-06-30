c105d896f69d9856685973a2de50e7ea15dde2eb
Alexey Kachayev <kachayev@gmail.com>
2015-06-30 18:43:29 +0300

    Remove unnecessary dependencies (cider)

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :global-vars {*warn-on-reflection* false}
-  :plugins [[cider/cider-nrepl "0.8.2"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cats "0.4.0"]]
